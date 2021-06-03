43f4824523ce237f8c48bb36dbce22f3d83f7502
sihwan0Lee <stay136@gmail.com>
2021-06-03 19:49:53 +0900

    +logout

--- shgram/apis/urls.py
+++ shgram/apis/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from apis.views import UserCreateView, UserLoginView
+from apis.views import UserCreateView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('users/create/', UserCreateView.as_view(), name='user_create'),
-    path('users/login', UserLoginView.as_view(), name='user_login')
-
+    path('users/login', UserLoginView.as_view(), name='user_login'),
+    path('users/logout', UserLogoutView.as_view(), name='user_logout')
 
 ]
