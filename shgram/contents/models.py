c290f6a2d816898f57659c36ef5f1137d4e605fb
sihwan0Lee <stay136@gmail.com>
2021-06-07 08:28:25 +0900

    +static, base.html

--- shgram/contents/models.py
+++ shgram/contents/models.py
@@ -11,17 +11,6 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-    # 이 클래스 자체가 추상화클래스가 되어  클래스의 클래스라고 생각하자.
-    # 메타 옵션에서 abstract = True 를 설정하면 엄마 모델은 실제로 또는
-    # 물리적으로 존재하지 않는 가상의 클래스가 된다. 그리고 새끼 모델들은 엄마의 필드와 속성,
-    # 함수들을 다 물려받아 실체가 있는 DB 테이블이 된다.
-    # 그니까 상속관계 없이 자식이 독립적이게 된다는거임.
-    # ❯ python manage.py makemigrations
-    #   Migrations for 'contents':
-    #   contents/migrations/0001_initial.py
-    #   - Create model Content
-    #   - Create model Image
-    # 보셈 베이스모델은 언급되지않음
 
 
 class Content(BaseModel):
